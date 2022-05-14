d0001146617074cdd84f13fb03d05a6e82e8b7b7
OceanLeonAI <beyondjayfir@126.com>
2022-05-14 23:12:24 +0800

    init the repository

---
+++ src/test/java/com/leon/helloemail/HelloEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.helloemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
